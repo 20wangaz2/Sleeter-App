58b66626de4d09e85914e874d801bb249bfcbfdb
Andrew Wang <tur65606@temple.edu>
2025-10-24 15:55:58 +0900

    Initial commit

---
+++ Sleeter/Sleeter/SleeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleeterApp.swift
+//  Sleeter
+//
+//  Created by Andrew Wang on R 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SleeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sleeter/SleeterTests/SleeterTests.swift
@@ -0,0 +1,17 @@
+//
+//  SleeterTests.swift
+//  SleeterTests
+//
+//  Created by Andrew Wang on R 7/10/24.
+//
+
+import Testing
+@testable import Sleeter
+
+struct SleeterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
